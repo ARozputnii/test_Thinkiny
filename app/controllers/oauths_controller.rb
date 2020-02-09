4b71d6a65aa5e29e8a45fc4481e85d5cc70bd1bf
Andrii <a.rozputnii@gmail.com>
2020-02-09 17:26:25 +0200

    7

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -1,13 +1,14 @@
 class OauthsController < ApplicationController
+  skip_before_filter :require_login
 
   # sends the user on a trip to the provider,
   # and after authorizing there back to the callback url.
   def oauth
-    login_at(params[:provider])
+    login_at(auth_params[:provider])
   end
 
   def callback
-    provider = params[:provider]
+    provider = auth_params[:provider]
     if @user = login_from(provider)
       redirect_to root_path, :notice => "Logged in from #{provider.titleize}!"
     else
@@ -23,4 +24,10 @@ class OauthsController < ApplicationController
       end
     end
   end
+
+  private
+    def auth_params
+    params.permit(:code, :provider)
+  end
+
 end
